11d25a29ccab379ff7ff14df313ca94baf0a4a6f
apexcoder007 <saurabhdubey96440@gmail.com>
2025-06-09 01:26:34 +0530

    added a function to instantiate a new server

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"net/http/httputil"
+	"net/url"
 )
 
 type simpleServer struct {
 	addr  string
 	proxy httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverURL, err := url.Parse(addr)
+	handleError(err)
+
+	return &simpleServer{
+		addr:  addr,
+		proxy: *httputil.NewSingleHostReverseProxy(serverURL),
+	}
 }
